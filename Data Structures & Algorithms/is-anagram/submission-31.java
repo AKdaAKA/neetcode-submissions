3b7f389772c3439707b5dc7a0b6bf3a87ab1b455
Aakash <48263062+AKdaAKA@users.noreply.github.com>
2026-06-11 14:49:43 -0500

    Add: is-anagram - submission-31

---
+++ Data Structures & Algorithms/is-anagram/submission-31.java
@@ -0,0 +1,33 @@
+class Solution {
+    char temp;
+    public boolean isAnagram(String s, String t) {
+    
+     String letters = "abcdefghijklmnopqrstuvwxyz";
+
+        int[] charCountersS = new int[26];
+        int[] charCountersT = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 0; j < 26; j++){
+                //a
+            if(letters.charAt(j) == s.charAt(i))
+            {
+                charCountersS[j]++; 
+            } 
+         }
+        }
+        for (int i = 0; i < t.length(); i++) {//for each char
+            for (int j = 0; j < 26; j++){ //all 26 chars
+           if(letters.charAt(j) == t.charAt(i))
+           {
+             charCountersT[j]++; 
+           } 
+         }
+        }
+        if (Arrays.equals(charCountersS, charCountersT))
+        {
+            return true;
+        }
+    
+        return false;
+    }
+}
