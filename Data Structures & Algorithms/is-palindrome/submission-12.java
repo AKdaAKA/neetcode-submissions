41e74a5d83e4aa2fe38e9d71766b3648b4199e20
Aakash <48263062+AKdaAKA@users.noreply.github.com>
2026-07-24 18:21:56 -0500

    Add: is-palindrome - submission-12

---
+++ Data Structures & Algorithms/is-palindrome/submission-12.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        char[] palindromeNorm = new char[s.length()];
+        char[] palindromeStuff = new char[s.length()];
+        int iterator = 0;
+        int otherIterator = 0;
+        for (int i = 0; i < s.length(); i++)
+        {
+            
+            if (!(s.charAt(i) == ':' || s.charAt(i) == '\'' || s.charAt(i) == ' ' || s.charAt(i) == '!' || s.charAt(i) == '?'|| s.charAt(i) == '.'|| s.charAt(i) == ','))
+            {
+                palindromeNorm[otherIterator] = s.charAt(i);
+                otherIterator++;
+            }
+        }
+        for (int i = s.length()-1; i >= 0; i--)
+        {
+            
+            if (!(s.charAt(i) == ':' || s.charAt(i) == '\'' || s.charAt(i) == ' ' || s.charAt(i) == '!' || s.charAt(i) == '?'|| s.charAt(i) == '.'|| s.charAt(i) == ','))
+            {
+                palindromeStuff[iterator] = s.charAt(i);
+    
+                iterator++;
+            }
+        }
+
+
+        String normString = String.valueOf(palindromeNorm).toLowerCase();
+        String palinString = String.valueOf(palindromeStuff).toLowerCase();
+
+        
+        if (normString.equals(palinString))
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
