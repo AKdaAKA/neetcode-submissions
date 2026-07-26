0d4a25a5e4ed7f7e66c368516c2d331675b55fe0
Aakash <48263062+AKdaAKA@users.noreply.github.com>
2026-07-26 15:18:53 -0500

    Add: three-integer-sum - submission-4

---
+++ Data Structures & Algorithms/three-integer-sum/submission-4.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+         List<List<Integer>> resultList = new ArrayList<>();
+        
+        for (int i = 0; i < nums.length; i++)
+        {
+            for (int j = i; j < nums.length; j++)
+            {
+                for (int k = j; k < nums.length; k++)
+                {
+                    if (nums[i] + nums[j] + nums[k] == 0 && (i!=j) && (j!=k) && (i!=k))
+                    {
+                        if (!(resultList.contains(List.of(nums[i],nums[j],nums[k])) || 
+                        resultList.contains(List.of(nums[j],nums[i],nums[k])) ||
+                        resultList.contains(List.of(nums[i],nums[k],nums[j])) ||
+                        resultList.contains(List.of(nums[k],nums[i],nums[j])) ||
+                        resultList.contains(List.of(nums[j],nums[k],nums[i])) ||
+                        resultList.contains(List.of(nums[k],nums[j],nums[i]))))
+                            resultList.add(new ArrayList<>(List.of(nums[i],nums[j],nums[k])));
+                        
+                    }
+                }
+            }
+        }
+        return resultList;
+    }
+}
