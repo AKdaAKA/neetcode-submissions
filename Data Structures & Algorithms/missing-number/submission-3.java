accee24842c0288b552aaad02bf87164ff4fa851
Aakash <48263062+AKdaAKA@users.noreply.github.com>
2026-08-14 22:39:16 -0500

    Add: missing-number - submission-3

---
+++ Data Structures & Algorithms/missing-number/submission-3.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        //3, 0 , 1
+        //
+        for (int i = 0; i < nums.length; i++)
+        {
+            Arrays.sort(nums);
+            System.out.println(nums[i]);
+            if (nums[0] != 0)
+            {
+                return 0;
+            }
+            if (i+1 < nums.length) {
+            if (nums[i]+1 != nums[i+1]) //1 != 2
+            {
+                return nums[i]+1;
+            }
+            
+            }
+            //1 2 3
+            //0
+        }
+        return nums[nums.length - 1]+1;
+    }
+}
