a176037ffe7ed81721352cc0d16694a724bfaa99
Aakash <48263062+AKdaAKA@users.noreply.github.com>
2026-08-15 19:27:30 -0500

    Add: single-number - submission-1

---
+++ Data Structures & Algorithms/single-number/submission-1.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+
+        
+        // int total = 0;
+        // boolean found = false;
+        // for (int i = 0; i < nums.length; i++)
+        // {
+        //     for (int j = 0; j < nums.length; j++)
+        //     {
+        //         if (nums[i] == nums[j] && i!=j) //
+        //         {
+        //             found = true;
+        //         }
+        //     }
+        //     if (!found)
+        //     {
+        //         return nums[i];
+        //     }
+        //     found = false;
+        // }
+        // return -1;
+
+         HashSet<Integer> numbers = new HashSet<Integer>();
+         for (int i = 0; i < nums.length; i++)
+         {
+            
+            if (numbers.contains(nums[i]))
+            {
+                numbers.remove(nums[i]);
+            }
+            else {
+                numbers.add(nums[i]);
+            }
+            
+         }
+         return numbers.iterator().next();
+    }
+}
